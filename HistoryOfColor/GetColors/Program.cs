59b144375271111520d59e6e3b162dc6b68ba150
Neel Dey <indraneel.dey@gmail.com>
2022-12-15 15:18:11 -0800

    Now using KMeans to get colors
    Split the images into evenly sliced blocks for faster processing. It uses ML KMeans to extract colors, up to 5 per block and accumulates them into a common list

---
+++ HistoryOfColor/GetColors/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+using Accord.MachineLearning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DirectoryInfo dir = new("./squares");
+        FileInfo[] files = dir.GetFiles();
+
+        KMeans kmeans = new KMeans(4);
+
+        // for (int i = 0; i < files.Length; i++)
+        List<string> colorListing = new();
+        for (int i = 0; i < 5; i++)
+        {
+            Bitmap map = new Bitmap(files[i].FullName);
+            Color[] colors = new Color[map.Width * map.Height];
+            for (int j = 0; j < map.Width; j++)
+            {
+                for (int k = 0; k < map.Height; k++)
+                {
+                    colors[j * map.Width + k] = map.GetPixel(j, k);
+                }
+            }
+            
+            double[][] data = colors.Select(color => new double[] { color.R, color.G, color.B }).ToArray();
+            KMeansClusterCollection clusters = kmeans.Learn(data);
+            double[][] centroids = clusters.Centroids;
+
+            colorListing.AddRange(from centeriod in centroids
+                let val1 = Convert.ToInt32(Math.Ceiling(centeriod[0]))
+                let val2 = Convert.ToInt32(Math.Ceiling(centeriod[1]))
+                let val3 = Convert.ToInt32(Math.Ceiling(centeriod[2]))
+                select $"{val1:x}{val2:x}{val3:x}");
+        }
+        
+        foreach (var colorFound in colorListing.Distinct())
+        {
+            Console.WriteLine(colorFound);
+        }
+    }
+}
---
+++ HistoryOfColor/ImageSplit/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+using Emgu.CV;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string imagePath = "image/young_woman_with_a_butterfly_1980.62.40.jpg";
+
+        Mat imageMat = CvInvoke.Imread(imagePath);
+        int squareWidth = imageMat.Width / 100;
+        int squareHeight = imageMat.Height / 100;
+        
+        Mat square = new();
+        
+        for (int x = 0; x < 100; x++)
+        {
+            for (int y = 0; y < 100; y++)
+            {
+                int xPos = x * squareWidth;
+                int yPos = y * squareHeight;
+                square = new(imageMat, new Rectangle(xPos, yPos, squareWidth, squareHeight));
+                square.Save($"squares/{x}_{y}.jpg");
+            }
+        }
+        
+    }
+}
